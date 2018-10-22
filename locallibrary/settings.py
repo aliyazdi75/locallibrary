bd7cb61224c4110c88219b4cbdbe453b8120d488
aliyazdi75 <ma.yazdi75@gmail.com>
2018-10-22 23:18:21 +0330

    add classes model that coded before

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'catalog.apps.AppConfig',
+    'catalog.apps.CatalogConfig',
 ]
 
 MIDDLEWARE = [
