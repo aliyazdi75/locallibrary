fa3eba03a2c85cc97857c1e1b0382c8439802d55
aliyazdi75 <ma.yazdi75@gmail.com>
2019-01-18 14:07:14 +0330

    add admin page

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,4 +4,6 @@ from . import views
 app_name = 'catalog'
 urlpatterns = [
     path('', views.Index.as_view(), name='index'),
+    path('books/', views.BookListView.as_view(), name='books'),
+    path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
 ]
