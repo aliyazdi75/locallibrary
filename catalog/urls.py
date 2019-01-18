ca70f69a26f1426403ef9fbd6141ba3478ebc4c4
aliyazdi75 <ma.yazdi75@gmail.com>
2019-01-18 17:55:33 +0330

    add authors page and views and update and delete authors

--- catalog/urls.py
+++ catalog/urls.py
@@ -7,5 +7,10 @@ urlpatterns = [
     path('books/', views.BookListView.as_view(), name='books'),
     path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
     path('authors/', views.AuthorListView.as_view(), name='authors'),
+    path('authors/<int:pk>/', views.AuthorDetailView.as_view(), name='authors-detail'),
     path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
+    path('book/<int:pk>/renew/', views.renew_book_librarian, name='renew-book-librarian'),
+    path('^author/create/', views.AuthorCreate.as_view(), name='author_create'),
+    path('^author/<int:pk>/update/', views.AuthorUpdate.as_view(), name='author_update'),
+    path('^author/<int:pk>/delete/', views.AuthorDelete.as_view(), name='author_delete'),
 ]
