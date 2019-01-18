e29a54169bb53742ba9ad8b8a47881519dc218b8
aliyazdi75 <ma.yazdi75@gmail.com>
2019-01-18 21:00:31 +0330

    fix some bugs \

--- catalog/urls.py
+++ catalog/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('authors/<int:pk>/', views.AuthorDetailView.as_view(), name='authors-detail'),
     path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
     path('book/<int:pk>/renew/', views.renew_book_librarian, name='renew-book-librarian'),
-    path('^author/create/', views.AuthorCreate.as_view(), name='author_create'),
-    path('^author/<int:pk>/update/', views.AuthorUpdate.as_view(), name='author_update'),
-    path('^author/<int:pk>/delete/', views.AuthorDelete.as_view(), name='author_delete'),
+    path('author/create/', views.AuthorCreate.as_view(), name='author_create'),
+    path('author/<int:pk>/update/', views.AuthorUpdate.as_view(), name='author_update'),
+    path('author/<int:pk>/delete/', views.AuthorDelete.as_view(), name='author_delete'),
 ]
--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -55,11 +55,10 @@ ROOT_URLCONF = 'locallibrary.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'DIRS': ['./templates'],
         'APP_DIRS': True,
         'OPTIONS': {
-            'context_p.rocessors': [
+            'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
